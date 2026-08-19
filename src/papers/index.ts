a83db54c839ba3d5260e7e2f91eb681135c99881
matquant <matquant3@gmail.com>
2026-08-19 13:47:35 -0400

    fixed protal

--- src/papers/index.ts
+++ src/papers/index.ts
@@ -1,10 +1,10 @@
 import type { ResearchPaper } from '../data/papers';
-import { hrpOptimization } from './hrp-optimization';
+
 import { maCrossover } from './ma-crossover';
 import { denoising } from './denoising';
 
 export const RESEARCH_PAPERS: ResearchPaper[] = [
   maCrossover,
-  hrpOptimization,
+
   denoising,
 ];
